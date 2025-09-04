f94955b1e99756a8dacf6f17d3556d173bc6f2dd
Diego Chan <xdiego.chanx@gmail.com>
2025-09-04 14:42:19 -0500

    feat (project) Empty SvelteKit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "$styles/index.css";
+
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>Home | AniDex</title>
+</svelte:head>
+
+{@render children?.()}
